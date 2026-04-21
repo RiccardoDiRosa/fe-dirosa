ade683d95f4483b940f2e23088ad06a6045973c7
riccardo (Riccardo Di Rosa) <new.riccardo.dirosa@gmail.com>
2026-04-21 12:08:47 +0200

    feat completato gli eseercizi di indroduzioni 01

---
+++ js/esercizi/introduzione01.js
@@ -0,0 +1,55 @@
+// 1
+// console.log("Riccardo Di Rosa");
+// 2
+console.log("riccardo")
+console.log("19")
+console.log("arese")
+// 3
+console.log(`${15 +27}`)
+console.log(`${100 - 58}`)
+console.log(`${7*8}`)
+console.log(`${144/12}`)
+// 4
+console.log("Mela", "Pera", "Banana");
+// 5
+// Questo è un commento su una riga sola
+
+/* Questo è un commento
+   su più righe (multi-riga)
+   molto utile per spiegazioni lunghe */
+
+console.log("Ciao! Questo programma contiene dei commenti nel codice.");
+// 6
+console.log("Ciao, sono Riccardo.");
+console.log("vivo ad arese");
+console.log("gioco a calcio");
+// 7
+console.log("Riccardo");
+console.log("Riccardo");
+console.log("Riccardo");
+console.log("Riccardo");
+console.log("Riccardo");
+
+// 8
+console.log("******************************");
+console.log("          Riccardo            ");
+console.log("******************************");
+//9
+console.log("========================");
+console.log("    RICEVUTA DI PAGAMENTO");
+console.log("========================");
+console.log("Prodotto: Pizza Margherita");
+console.log("Quantità: 2");
+console.log("Prezzo: €14.00");
+console.log("========================");
+// 10
+console.log(10 + 5 * 2);   // Risultato: 20
+console.log((10 + 5) * 2); // Risultato: 30
+
+//11
+console.log(0.1 + 0.2);
+
+/* Il risultato è 0.30000000000000004.
+   Non è esattamente 0.3 come ci si aspetterebbe!
+
+*/
---
+++ js/main.js
@@ -0,0 +1,11 @@
+//commento
+console.log("Ciao")
+/**
+ * 
+ * 
+ * 
+ * console.log("ho", 10, "anni")
+ * 
+ */
+
+console.log(`ho ${3 +30} anni`) 
