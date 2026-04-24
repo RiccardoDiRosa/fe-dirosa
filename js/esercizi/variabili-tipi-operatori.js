d434b43df67ce9572d9d27c4a050c73cea7dc6ca
riccardo (Riccardo Di Rosa) <new.riccardo.dirosa@gmail.com>
2026-04-24 09:44:36 +0200

    feat esercizio 2.8

--- js/esercizi/variabili-tipi-operatori.js
+++ js/esercizi/variabili-tipi-operatori.js
@@ -35,11 +35,21 @@ let numero = +stringa + 7;
 
 console.log(numero); // Risultato: 130
 
-//esercizio 2.6
+//esercizio 2.7
 let eta = 17;
 let hapermesso = true;
 if(eta>=18 || hapermesso){
     return true;
     console.log(eta,hapermesso);
 }
+
+//esercizio 2.8
+let nome = "Mario";
+let cognome = "Rossi";
+let eta = 25;
+
+// Uso del template literal
+let presentazione = `Ciao, mi chiamo ${nome} ${cognome} e ho ${eta} anni.`;
+
+console.log(presentazione);
 
