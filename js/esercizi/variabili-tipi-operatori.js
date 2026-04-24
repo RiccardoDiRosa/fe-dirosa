58871784e69bddcd30cab42387b587df877fb655
riccardo (Riccardo Di Rosa) <new.riccardo.dirosa@gmail.com>
2026-04-24 09:29:17 +0200

    feat primi due esercizi

---
+++ js/esercizi/variabili-tipi-operatori.js
@@ -0,0 +1,13 @@
+// esercizio 2.1
+let nome = "Riccardo";
+const annoNascita = 2006;
+console.log(nome + annoNascita);
+
+//esercizio 2.2
+let a = 3, b = 4;
+console.log(a+b);
+console.log(a-b);
+console.log(a/b);
+console.log(a*b);
+
+//esercizio 2.3
